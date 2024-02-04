d021d9f6b58f1ea20f5e79a0ee1b976dd00e4ad9
lucaslnrr <lucas.lnrr@gmail.com>
2024-02-04 17:38:21 -0300

    testee

--- server.js
+++ server.js
@@ -51,3 +51,4 @@ app.post('/upload', upload.single('csvFile'), async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+module.exports = app;
